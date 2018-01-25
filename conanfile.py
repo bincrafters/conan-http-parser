79f793446e7b68d393bd528c7ecb80fd48fe7ff9
theirix <theirix@gmail.com>
2018-01-25 18:45:02 +0300

    Version 2.7.1p1

--- conanfile.py
+++ conanfile.py
@@ -3,10 +3,10 @@ import os
 
 class HttpParserConan(ConanFile):
     name = "http-parser"
-    version = "2.7.1"
+    version = "2.7.1p1"
     description = "http request/response parser for c"
     url = "https://github.com/theirix/conan-http-parser"
-    license = "https://github.com/nodejs/http-parser/blob/release-%s/LICENSE-MIT" % version
+    license = "MIT"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=False"
@@ -16,8 +16,9 @@ class HttpParserConan(ConanFile):
         del self.settings.compiler.libcxx
 
     def source(self):
-        tools.get("https://github.com/nodejs/http-parser/archive/v%s.tar.gz" % (self.version))
-        os.rename('http-parser-%s' % (self.version), self.name)
+        upstream_ver = self.versions.split("p")[0]
+        tools.get("https://github.com/nodejs/http-parser/archive/v%s.tar.gz" % upstream_ver)
+        os.rename('http-parser-%s' % upstream_ver, self.name)
 
     def build(self):
         tools.patch(patch_file=os.path.join(self.build_folder, 'http-parser.patch'),
@@ -36,6 +37,7 @@ class HttpParserConan(ConanFile):
                     self.run(cmd)
 
     def package(self):
+        self.copy("license*", dst="licenses", src="%s" % (self.name), ignore_case=True, keep_path=False)
         self.copy("*.h", dst="include", src="%s/distr/include" % (self.name))
         if self.options.shared:
             if self.settings.os == "Macos":
