e809000d5adf9915f04e761b1830f4334f3501df
theirix <theirix@gmail.com>
2018-05-03 16:15:00 +0300

    CMake-based build with custom CMakeLists.txt, #1

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,28 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
+import shutil
+from conans import ConanFile, CMake, tools
 
 class HttpParserConan(ConanFile):
     name = "http-parser"
-    version = "2.7.1p1"
+    version = "2.8.1"
     description = "http request/response parser for c"
-    url = "https://github.com/theirix/conan-http-parser"
+    url = "https://github.com/bincrafters/conan-http-parser"
+    homepage = "https://github.com/nodejs/http-parser"
     license = "MIT"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
-    exports = ["LICENSE.md", "%s.patch" % name]
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = "shared=False", "fPIC=True"
+    exports = "LICENSE.md"
+    exports_sources = "CMakeLists.txt"
+    generators = "cmake"
+    source_subfolder = "source_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.remove("fPIC")
 
     def configure(self):
         del self.settings.compiler.libcxx
@@ -18,35 +30,24 @@ class HttpParserConan(ConanFile):
     def source(self):
         upstream_ver = self.version.split("p")[0]
         tools.get("https://github.com/nodejs/http-parser/archive/v%s.tar.gz" % upstream_ver)
-        os.rename('http-parser-%s' % upstream_ver, self.name)
+        os.rename('http-parser-%s' % upstream_ver, self.source_subfolder)
+
+        shutil.copy("CMakeLists.txt",
+                    os.path.join(self.source_subfolder, "CMakeLists.txt"))
 
     def build(self):
-        tools.patch(patch_file=os.path.join(self.build_folder, 'http-parser.patch'),
-                    base_path=self.name)
-
-        env_build = AutoToolsBuildEnvironment(self)
-
-        if self.settings.os == "Linux" or self.settings.os == "Macos":
-
-            target = 'install' if self.options.shared else 'install-static'
-
-            with tools.environment_append(env_build.vars):
-                with tools.chdir(self.name):
-                    cmd = 'make PREFIX=distr CFLAGS_FAST_EXTRA=-Wno-error CFLAGS_DEBUG_EXTRA=-Wno-error %s' % (target)
-                    self.output.warn(cmd)
-                    self.run(cmd)
+        cmake = CMake(self)
+        if self.settings.os != "Windows":
+            cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
+        cmake.definitions['WITH_CONAN'] = True
+        cmake.definitions['WITH_TESTS'] = False
+        # BUILD_SHARED_LIBS is set automatically
+        cmake.configure(source_folder=self.source_subfolder)
+        cmake.build()
+        cmake.install()
 
     def package(self):
-        self.copy("license*", dst="licenses", src="%s" % (self.name), ignore_case=True, keep_path=False)
-        self.copy("*.h", dst="include", src="%s/distr/include" % (self.name))
-        if self.options.shared:
-            if self.settings.os == "Macos":
-                self.copy(pattern="%s/distr/lib/*.dylib" % self.name, dst="lib", keep_path=False)
-            else:
-                self.copy(pattern="%s/distr/lib/*.so*" % self.name, dst="lib", keep_path=False)
-        else:
-            self.copy(pattern="%s/distr/lib/*.a" % self.name, dst="lib", keep_path=False)
+        self.copy("LICENSE-MIT", dst="licenses", src=self.source_subfolder, ignore_case=True, keep_path=False)
 
     def package_info(self):
-        if self.settings.os == "Linux" or self.settings.os == "Macos":
-            self.cpp_info.libs = ['http_parser']
+        self.cpp_info.libs = tools.collect_libs(self)
